25880bc63d63380c65c3a2ea1ec93642d3bf77e7
DimaOlenych <olenychdima@gmail.com>
2017-02-24 17:25:03 +0200

    Delegates

---
+++ Delegates/Delegates01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Delegates
+{
+    static class MyClass
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Метод сообщенный с делегатом");
+        }
+    }
+
+    public delegate void MyDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = new MyDelegate(MyClass.Method);
+            myDelegate.Invoke();
+            myDelegate();
+        }
+    }
+}
---
+++ Delegates/Delegates02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegates02
+{
+    class MyClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод сообщенный с делегатом");
+        }
+    }
+
+    public delegate void MyDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            MyDelegate myDelegate = new MyDelegate(instance.Method);
+            myDelegate.Invoke();
+            myDelegate();
+        }
+    }
+}
